17359b4490f1f8d37c70492009512772ddfe1d39
Suyash Phatak <suyashphatakbackup@gmail.com>
2023-01-31 18:00:54 +0530

    Started login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   # signup route
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  # login route
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end 
