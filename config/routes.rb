dd16c3a3ee0ed3efdcc17211539d48825fc4af15
Suyash Phatak <suyashphatakbackup@gmail.com>
2023-01-31 12:00:47 +0530

    Added signup form without its functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # Defines the root path route ("/")
+  # Articles routes
   root "articles#home"
-  
   resources :articles
 
+  # signup route
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end 
