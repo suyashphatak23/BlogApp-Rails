841507f44ecf677d79b9a6458092bbbe662bbdde
Suyash Phatak <suyashphatakbackup@gmail.com>
2023-02-10 17:52:15 +0530

    Updated navigation UI and did rich text research

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,7 @@ class Article < ApplicationRecord
     has_many :categories, through: :article_categories
     has_one_attached :banner, :dependent => :destroy
     has_one_attached :thumbnail, :dependent => :destroy
-    has_rich_text :body
+    has_rich_text :content
 
     validates :title, presence:true
     validates :body, presence:true, length:{ minimum: 5 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_08_062731) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_110316) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_08_062731) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "content"
   end
 
   create_table "categories", force: :cascade do |t|
