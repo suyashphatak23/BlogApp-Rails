fcc57d76a9d5b811be95483c1c01db4d5a0fb97c
Suyash Phatak <suyashphatakbackup@gmail.com>
2023-02-15 11:30:01 +0530

    Updated UI

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,55 +1,55 @@
+# frozen_string_literal: true
+
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: %i[index show]
 
-    before_action :require_admin, except: [:index, :show]
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
 
-    def index
-        @categories = Category.paginate(page: params[:page], per_page: 5)
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = 'Category Created Successfully'
+      redirect_to @category
+    else
+      render 'new'
     end
+  end
 
-    def new
-        @category = Category.new
+  def show
+    @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = 'Category name updated successfully'
+      redirect_to @category
+    else
+      render 'edit'
     end
+  end
 
-    def create
-        @category = Category.new(category_params)
-        if @category.save
-            flash[:notice] = "Category Created Successfully"
-            redirect_to @category
-        else
-            render 'new'
-        end
-    end
+  private
 
-    def show
-        @category = Category.find(params[:id])
-        @articles = @category.articles.paginate(page: params[:page], per_page: 5)
-    end
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
-    def edit
-        @category = Category.find(params[:id])
-    end
+  def require_admin
+    return if logged_in? && current_user.admin?
 
-    def update
-        @category = Category.find(params[:id])
-        if @category.update(category_params)
-            flash[:notice] = "Category name updated successfully"
-            redirect_to @category
-        else
-            render 'edit'
-        end
-    end
-
-    private
-
-    def category_params
-        params.require(:category).permit(:name)
-    end
-
-    def require_admin
-        if !(logged_in? && current_user.admin?)
-          flash[:alert] = "Only admins can perform that action"
-          redirect_to categories_path
-        end
-    end
+    flash[:alert] = 'Only admins can perform that action'
+    redirect_to categories_path
+  end
 end
-
