fbda6321c0f4cd4423e91c439cec72e671e45e60
Suyash Phatak <suyashphatakbackup@gmail.com>
2023-02-17 17:32:14 +0530

    Updated Detailed Article View, tried footer, updated login views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 class Article < ApplicationRecord
-    belongs_to :user
-    has_many :article_categories
-    has_many :categories, through: :article_categories
-    has_one_attached :banner, :dependent => :destroy
-    has_one_attached :thumbnail, :dependent => :destroy
-    has_rich_text :content
+  belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  has_one_attached :banner, dependent: :destroy
+  has_one_attached :thumbnail, dependent: :destroy
+  has_rich_text :content
 
-    validates :title, presence:true
-    validates :body, presence:true, length:{ minimum: 5 }
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 5 }
 
-    def thumbnail_resize
-        thumbnail.variant(resize_to_limit: [300,300]).processed
-    end
+  def thumbnail_resize
+    thumbnail.variant(resize_to_limit: [300, 300]).processed
+  end
 
-    def banner_resize
-        banner.variant(resize_to_limit: [500,500]).processed
-    end
+  def banner_resize
+    banner.variant(resize_to_limit: [800, 700]).processed
+  end
 end
